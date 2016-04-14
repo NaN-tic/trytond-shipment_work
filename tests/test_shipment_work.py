abac9b329bd5420b9f7c50ebf73a64fa332052b3
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-04-14 11:24:37 +0200

    Fix module instalation

--- tests/test_shipment_work.py
+++ tests/test_shipment_work.py
@@ -4,23 +4,13 @@
 import unittest
 import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('shipment_work')
-
-    def test0005views(self):
-        'Test views'
-        test_view('shipment_work')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'shipment_work'
 
 
 def suite():
