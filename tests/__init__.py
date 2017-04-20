75473aca5a7ec86d27b406fcfe23f89065314dce
Raimon Esteve <raimon@nan-tic.com>
2017-04-20 15:05:16 +0200

    Clean default branch

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-
-try:
-    from trytond.modules.shipment_work.test_shipment_work import suite
-except ImportError:
-    from .test_shipment_work import suite
-
-__all__ = ['suite']
